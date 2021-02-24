7836e5f160cab51d8e6bdd7d04c4ac7d484ac9d9
Микола Матіос <mikolka09@gmail.com>
2021-02-24 13:30:48 +0200

    Добавьте файлы проекта.

---
+++ DancingProgressBar/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace DancingProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxProgressBar = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(171, 40);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(82, 29);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(50, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество ПрогрессБаров";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(264, 637);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxProgressBar
+            // 
+            this.comboBoxProgressBar.FormattingEnabled = true;
+            this.comboBoxProgressBar.Location = new System.Drawing.Point(72, 45);
+            this.comboBoxProgressBar.Name = "comboBoxProgressBar";
+            this.comboBoxProgressBar.Size = new System.Drawing.Size(68, 24);
+            this.comboBoxProgressBar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 678);
+            this.Controls.Add(this.comboBoxProgressBar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxProgressBar;
+    }
+}
+
---
+++ DancingProgressBar/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace DancingProgressBar
+{
+    public partial class Form1 : Form
+    {
+        delegate void Progress(int value);
+        public int countBar = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 13; i++)
+            {
+                comboBoxProgressBar.Items.Add(i);
+            }
+        }
+
+        public Color RandColor()
+        {
+            Random randGen = new Random();
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            KnownColor randColorName = names[randGen.Next(names.Length)];
+            Color randomColor = Color.FromKnownColor(randColorName);
+            Thread.Sleep(20);
+            return randomColor;
+        }
+
+        public void DeleteBar()
+        {
+            List<Control> Con = new List<Control>();
+            foreach (Control item in this.Controls)
+            {
+                if ((item as ProgressBar) != null)
+                    if ((item as ProgressBar) == item)
+                        Con.Add(item);
+            }
+            foreach (Control item in Con)
+            {
+                this.Controls.Remove(item);
+            }
+        }
+
+        public void StartBar(object obj)
+        {
+
+            ProgressBar bar = (ProgressBar)obj;
+
+            if (bar.InvokeRequired)
+                bar.Invoke(new Action(() => bar.PerformStep()));
+        }
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            DeleteBar();
+            Random rand = new Random();
+            int x = 20; int y = 75;
+            countBar = Convert.ToInt32(comboBoxProgressBar.Text);
+            for (int i = 0; i < countBar; i++)
+            {
+                ProgressBar bar = new ProgressBar()
+                {
+                    Location = new Point(x, y),
+                    Minimum = 0,
+                    Maximum = 100,
+                    Value = rand.Next(100),
+                    ForeColor = RandColor(),
+                    Step = 5,
+                    Size = new Size(200, 30),
+                    Style = ProgressBarStyle.Continuous
+                };
+                y += 35;
+                this.Controls.Add(bar);
+            }
+            foreach (Control item in this.Controls)
+            {
+                if ((item as ProgressBar) != null)
+                {
+                    Thread thread = new Thread(StartBar);
+                    thread.IsBackground = true;
+                    thread.Start(item as ProgressBar);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
